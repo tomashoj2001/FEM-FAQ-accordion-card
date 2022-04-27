0b38cca05b1677a37c8a0a537bda4999c79cb36e
TomiHojnadel <92936927+tomashoj2001@users.noreply.github.com>
2022-04-27 09:04:33 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,44 @@
+const d = document,
+    blocks = [...d.querySelectorAll('.question-answer')],
+    questions = d.querySelectorAll('.info__question'),
+    arrows = d.querySelectorAll('.info__arrow'),
+    answers = d.querySelectorAll('.info__answer');
+
+let count = 0,
+    keepGoing = true,
+    index;
+
+function toggleAnswer() {
+    if (count === 0) {
+        index = this.dataset.index;
+        displayAnswer(index);
+    }
+
+    if (count > 0) {
+        if (index === this.dataset.index) {
+            displayAnswer(index);
+            keepGoing = false;
+        }
+        else {            
+            if (!keepGoing) {
+                keepGoing = true;
+                displayAnswer(this.dataset.index);
+            }    
+            else {
+                displayAnswer(index);
+                displayAnswer(this.dataset.index);
+            }
+        }
+        index = this.dataset.index;
+    }    
+    count += 1;
+}
+
+function displayAnswer(i) {
+    questions[i].classList.toggle('active');
+    arrows[i].classList.toggle('active');
+    answers[i].classList.toggle('hidden');
+}
+
+questions.forEach(question => question.addEventListener('click', toggleAnswer));
+arrows.forEach(arrow => arrow.addEventListener('click', toggleAnswer));
